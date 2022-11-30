d7069b485777f358ddb7ec974d05cc41502a36b6
AbigailDeng <Abigail.deng@ienyan.com>
2022-11-30 17:56:17 +0800

    feat: add pkg version diff logic

--- src/Helper/binary/index.ts
+++ src/Helper/binary/index.ts
@@ -1,21 +1,30 @@
 import { execSync } from "child_process";
 import { IdependenciesItem } from "../../../@types";
+import ora from "ora";
+const sourceSpinner = ora("");
 export const BinaryHelper = {
   upgradeChangedPkg: (
     pkgArr: IdependenciesItem[],
     devFlag?: boolean
   ): boolean => {
     try {
+      if (!pkgArr.length) {
+        sourceSpinner.info(
+          `no ${
+            devFlag ? "devDependencies" : "dependencies"
+          } package to upgrade`
+        );
+        return true;
+      }
       let pkgWithVer = "";
-      pkgArr.forEach((ele, index) => {
+      pkgArr.forEach((ele) => {
         let key = Object.keys(ele)[0],
           value = ele[key];
         let item = `${key}@${value}`;
         pkgWithVer += ` ${item}`;
       });
-      console.log(pkgWithVer);
       execSync(`yarn add ${pkgWithVer} ${devFlag ? "--dev" : ""}`, {
-        stdio: "ignore",
+        stdio: "inherit",
       });
       return true;
     } catch (e) {
--- src/Helper/diff/index.ts
+++ src/Helper/diff/index.ts
@@ -8,12 +8,95 @@ export const DiffHelper = {
   ): IdependenciesItem[] => {
     const res: IdependenciesItem[] = [];
     keys.forEach((ele) => {
-      if (prev[ele] !== curr[ele] && curr[ele]) {
+      if (DiffHelper.isUpgradePkg(curr, prev, ele)) {
         res.push({
           [ele]: curr[ele],
         });
       }
     });
     return res;
+  },
+  getPrefixAndVer: (
+    obj: IdependenciesItem,
+    key: string
+  ): {
+    prefix: string;
+    version: string;
+  } => {
+    const res = {
+      prefix: "",
+      version: "",
+    };
+    if (obj[key].startsWith("~")) {
+      res.prefix = "~";
+      res.version = obj[key].slice(1);
+    } else if (obj[key].startsWith("^")) {
+      res.prefix = "^";
+      res.version = obj[key].slice(1);
+    } else if (obj[key].startsWith("git+")) {
+      res.prefix = "git+";
+      res.version = obj[key];
+    } else {
+      res.version = obj[key];
+    }
+    return res;
+  },
+  // prefix ~
+  dealTilde: (verCurr: string, verPrev: string) => {
+    const verCurrArr = verCurr.split("."),
+      verPrevArr = verPrev.split(".");
+    if (
+      verPrevArr[0] === verCurrArr[0] &&
+      verPrevArr[1] === verCurrArr[1] &&
+      verPrevArr[2] >= verCurrArr[2]
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  // prefix ^
+  dealCaret: (verCurr: string, verPrev: string) => {
+    const verCurrArr = verCurr.split("."),
+      verPrevArr = verPrev.split(".");
+    if (verPrevArr[0] === verCurrArr[0] && verPrevArr[1] >= verCurrArr[1]) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+  isUpgradePkg: (
+    curr: IdependenciesItem,
+    prev: IdependenciesItem,
+    key: string
+  ): boolean => {
+    // remote not include
+    if (!curr[key]) {
+      return false;
+    }
+    if (!prev[key]) {
+      return true;
+    }
+    const { prefix: prefixCurr, version: verCurr } = DiffHelper.getPrefixAndVer(
+      curr,
+      key
+    );
+    const { prefix: prefixPrev, version: verPrev } = DiffHelper.getPrefixAndVer(
+      prev,
+      key
+    );
+    if (prefixCurr !== prefixPrev) {
+      return true;
+    } else {
+      switch (prefixCurr) {
+        case "~":
+          return DiffHelper.dealTilde(verCurr, verPrev);
+        case "^":
+          return DiffHelper.dealCaret(verCurr, verPrev);
+        default:
+          // no prefix or git+
+          return !(verCurr === verPrev);
+      }
+    }
   },
 };
